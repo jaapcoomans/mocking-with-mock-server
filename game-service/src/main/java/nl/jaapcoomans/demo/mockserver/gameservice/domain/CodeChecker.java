99e26082600b79f4a889dcba348c0bc8d729320d
Jaap Coomans <jaapcoomans@gmail.com>
2019-10-28 21:10:53 +0100

    Remove unused imports

--- game-service/src/main/java/nl/jaapcoomans/demo/mockserver/gameservice/domain/CodeChecker.java
+++ game-service/src/main/java/nl/jaapcoomans/demo/mockserver/gameservice/domain/CodeChecker.java
@@ -1,8 +1,5 @@
 package nl.jaapcoomans.demo.mockserver.gameservice.domain;
 
-import nl.jaapcoomans.demo.mockserver.gameservice.domain.Code;
-import nl.jaapcoomans.demo.mockserver.gameservice.domain.Result;
-
 public interface CodeChecker {
     Result checkCode(Code code, Code guess);
 }
